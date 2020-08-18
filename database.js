e34ab23a6778d5cf88ff75bae87622062f03ac6a
lizzethgd <lizzethgd@gmail.com>
2020-08-18 17:40:38 +0300

    fixing database file

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-const PORT = process.env.MONGODB_URI || 4000
+const URI  = process.env.MONGODB_URI
 
-mongoose.connect(PORT, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }, (err)=> {
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }, (err)=> {
     if (err) return console.log(err)
     console.log('The server is conncected to MongoDB database')
 })
